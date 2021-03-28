59d532daab4a47530d81ecf7d5871d13cbb3564e
W-yt <lalalu9514@gmail.com>
2021-03-28 14:00:19 +0800

    train文件添加说明

---
+++ main_project_py/train.py
@@ -0,0 +1,6 @@
+# ######################################################### #
+# Chinese chess robot upper-computer main project by python #
+# Create : 2021.3.28                                        #
+# Author : W-yt                                             #
+# File   : train                                            #
+# ######################################################### #
