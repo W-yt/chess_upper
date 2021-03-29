bf26edc1e2ff7c9eefc01eb93a9a17b49f345b09
W-yt <lalalu9514@gmail.com>
2021-03-29 17:29:26 +0800

    更新了C++的旋转代码，本来就ubuntu上更新了代码，但是没有push

--- image_rotate_c/main.cpp
+++ image_rotate_c/main.cpp
@@ -17,42 +17,64 @@ using namespace cv;
 
 string project_dir =  "/home/yutian/Project/chess_upper/";
 
+vector<string> PieceType;
+
+
 int main()
 {
+    PieceType.push_back("1-黑-車/");
+    PieceType.push_back("2-黑-卒/");
+    PieceType.push_back("3-黑-将/");
+    PieceType.push_back("4-黑-马/");
+    PieceType.push_back("5-黑-炮/");
+    PieceType.push_back("6-黑-士/");
+    PieceType.push_back("7-黑-象/");
+    PieceType.push_back("8-红-兵/");
+    PieceType.push_back("9-红-車/");
+    PieceType.push_back("10-红-马/");
+    PieceType.push_back("11-红-炮/");
+    PieceType.push_back("12-红-仕/");
+    PieceType.push_back("13-红-帥/");
+    PieceType.push_back("14-红-相/");
     Mat srcimage(Size(200,200),CV_8UC3);
     Mat dstimage(Size(200,200),CV_8UC3);
     Mat rotate_matrix;
-
-    string image_src_dir = project_dir + "原始_每种5张/" + "1-黑-車/";
-    string image_dst_dir = project_dir + "旋转_每种720张/" + "1-黑-車/";
     string image_name,output_name;
 
-    int num = 0;
-
-    for(int i = 1; i < 3; i++)
+    for(int type = 0; type < 14; type++)
     {
-        image_name = image_src_dir + to_string(i) + ".jpg";
+        string image_src_dir = project_dir + "原始_每种20张/" + PieceType.at(type);
+        string image_dst_dir = project_dir + "旋转_每种20x90张/" + PieceType.at(type);
 
-        srcimage = imread(image_name);
-        imshow("srcimage",srcimage);
+        int num = 0;
 
-        Point2f center = Point(srcimage.cols / 2, srcimage.rows / 2);
-
-        for(int angle = 1; angle < 361; angle++)
+        for(int i = 1; i < 21; i++)
         {
-            output_name = image_dst_dir + to_string(num) + ".jpg";
+            image_name = image_src_dir + to_string(i) + ".jpg";
 
-            rotate_matrix = getRotationMatrix2D(center,angle,1);
-            warpAffine(srcimage,dstimage,rotate_matrix,srcimage.size());
-            imshow("dstimage",dstimage);
+            srcimage = imread(image_name);
+            imshow("srcimage",srcimage);
 
-            imwrite(output_name,dstimage);
-            num++;
-            waitKey(1);
+            Point2f center = Point(srcimage.cols / 2, srcimage.rows / 2);
+
+            for(int angle = 1; angle < 361; angle+=4)
+            {
+                output_name = image_dst_dir + to_string(num) + ".jpg";
+
+                rotate_matrix = getRotationMatrix2D(center,angle,1);
+                warpAffine(srcimage,dstimage,rotate_matrix,srcimage.size());
+                imshow("dstimage",dstimage);
+
+                imwrite(output_name,dstimage);
+                num++;
+                waitKey(1);
+            }
+
+            waitKey(10);
         }
 
-        waitKey(10);
     }
+
 
     return 0;
 }
