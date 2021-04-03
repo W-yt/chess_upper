a399c87d589480618964af9e6d9c44efd9362292
W-yt <lalalu9514@gmail.com>
2021-04-03 17:45:12 +0800

    添加GUI文件（pyqt5）

--- main_project_py/board.py
+++ main_project_py/board.py
@@ -4,6 +4,7 @@
 # Author : W-yt                                             #
 # File   : board                                            #
 # ######################################################### #
+
 import cv2 as cv
 import numpy as np
 
--- main_project_py/piece.py
+++ main_project_py/piece.py
@@ -4,6 +4,7 @@
 # Author : W-yt                                             #
 # File   : piece                                            #
 # ######################################################### #
+
 import cv2 as cv
 import numpy as np
 from keras.models import load_model
